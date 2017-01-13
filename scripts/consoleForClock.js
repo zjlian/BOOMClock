9d491247c04076fdf03967b741fdee07bbb0e839
啊廉 <zjlianzjlianzjlian@outlook.com>
2017-01-13 19:48:51 +0800

    添加滑动条默认位置

--- scripts/consoleForClock.js
+++ scripts/consoleForClock.js
@@ -3,12 +3,18 @@ var ib = document.getElementById("button");
 
 var irrrr = document.getElementById("refreshRateRange");
 var irbcr = document.getElementById("ballsCacheRange");
+irrrr.defaultValue = refreshRate;
+irbcr.defaultValue = ballsCache;
 
 var irvxmr = document.getElementById("VXMinRange");
 var irvxxr = document.getElementById("VXMaxRange");
+irvxmr.defaultValue = V.xMin;
+irvxxr.defaultValue = V.xMax;
 
 var irvymr = document.getElementById("VYMinRange");
 var irvyxr = document.getElementById("VYMaxRange");
+irvymr.defaultValue = V.yMin;
+irvyxr.defaultValue = V.yMax;
 
 ib.onclick = function() {
 
